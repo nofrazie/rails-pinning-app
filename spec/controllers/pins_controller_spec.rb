888d6652e9102fee61c8f5286451f0ff41c9ebe0
Nolan Frazier <nofrazier3@gmail.com>
2016-03-03 16:56:19 -0800

    Added pin controller index test

--- spec/controllers/pins_controller_spec.rb
+++ spec/controllers/pins_controller_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 RSpec.describe PinsController do
 
+  describe "GET index" do
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+  end
+
 end
